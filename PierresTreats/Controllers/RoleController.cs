a22234221c6dd13d7767884038fbb5aed00b0891
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-02 15:50:20 -0700

    add role creation functionality.

--- PierresTreats/Controllers/RoleController.cs
+++ PierresTreats/Controllers/RoleController.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace PierresTreats.Controllers
 {
-  public class RoleController : Controller{
+  public class RoleController : Controller
+  {
     private RoleManager<IdentityRole> roleManager;
     public RoleController(RoleManager<IdentityRole> roleMgr)
     {
@@ -15,6 +18,26 @@ namespace PierresTreats.Controllers
     {
       foreach (IdentityError error in result.Errors)
         ModelState.AddModelError("", error.Description);
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create([Required] string name)
+    {
+      if (ModelState.IsValid)
+      {
+        IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          Errors(result);
+        }
+      }
+      return View(name);
     }
   }
 }
