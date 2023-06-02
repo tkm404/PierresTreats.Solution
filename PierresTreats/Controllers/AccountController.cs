f218967211e23d6e0f19834042db7bfeee6fe8d4
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-02 13:36:14 -0700

    WIP: Hunting a bug in the Register User view model (or view... or controller...)

--- PierresTreats/Controllers/AccountController.cs
+++ PierresTreats/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
 using PierresTreats.Models;
+using System.Threading.Tasks;
 using PierresTreats.ViewModels;
 
 namespace PierresTreats.Controllers
