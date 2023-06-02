824f377d71b0dae61a6a6d8ee734d7ee78ff1585
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-02 16:13:42 -0700

    update Role Controller with async-staggering solution for yesterday's roadblock.

--- PierresTreats/Controllers/RoleController.cs
+++ PierresTreats/Controllers/RoleController.cs
@@ -2,15 +2,21 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace PierresTreats.Controllers
 {
   public class RoleController : Controller
   {
     private RoleManager<IdentityRole> roleManager;
-    public RoleController(RoleManager<IdentityRole> roleMgr)
+    private UserManager<ApplicationUser> userManager;
+    public RoleController(RoleManager<IdentityRole> roleMgr, UserManager<ApplicationUser> userMgr)
     {
       roleManager = roleMgr;
+      userManager = userMgr;
     }
     public ViewResult Index() => View(roleManager.Roles);
 
@@ -39,5 +45,25 @@ namespace PierresTreats.Controllers
       }
       return View(name);
     }
+
+    public async Task<IActionResult> Update(string id)
+    {
+      IdentityRole role = await roleManager.FindByIdAsync(id);
+      List<ApplicationUser> allUsers = await userManager.Users.ToListAsync();
+
+      List<ApplicationUser> members = allUsers
+                                      .Where(user => userManager.IsInRoleAsync(user, role.Name).Result)
+                                      .ToList();
+      List<ApplicationUser> nonMembers = allUsers.Except(members).ToList();
+      return View(new RoleEdit
+      {
+        Role = role,
+        Members = members,
+        NonMembers = nonMembers
+      });
+
+    }
+
+
   }
 }
