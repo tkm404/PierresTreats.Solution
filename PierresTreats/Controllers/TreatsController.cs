8b5010615a81781d16859d2ddec51e4fe31a4526
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-02 09:48:59 -0700

    add TreatsController with CRUD functionality.

---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,160 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using PierresTreats.Models;
+
+namespace PierresTreats.Controllers
+{
+  [Authorize]
+  public class TreatsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public TreatsController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    // public async Task<ActionResult> Index()
+    // {
+    //   string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    //   ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+    //   List<Treat> model = _db.Treats
+    //                         .Where(entry => entry.User.Id == currentUser.Id)
+    //                         .Include(model => model.Property)
+    //                         .ToList();
+    //   return View(model);
+    // }
+
+    public ActionResult Index()
+    {
+      return View(_db.Treats.ToList());
+    }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+                              .Include(treat => treat.JoinEntities)
+                              .ThenInclude(join => join.Treat)
+                              .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+// READ functions ^^^^
+//------------------------------------------------------------------
+// CREATE functions vvvv
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    // [HttpPost]
+    // public async Task<ActionResult> Create(Treat treat)
+    // {
+    //   if (!ModelState.IsValid)
+    //   {
+    //       return View();
+    //   }
+    //   else
+    //   {
+    //     string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    //     ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+    //     treat.User = currentUser;
+    //     _db.Treats.Add(treat);
+    //     _db.SaveChanges();
+    //     return RedirectToAction("Index");
+    //   }
+    // }
+
+    public ActionResult Create(Treat treat)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View();
+      }
+      else
+      {
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult AddTreat(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddTreat(Treat treat, int flavorId)
+    {
+      #nullable enable
+      TreatFlavor? joinEntity = _db.TreatFlavors.FirstOrDefault(join => (join.FlavorId == flavorId && join.TreatId == treat.TreatId));
+      #nullable disable
+      if (joinEntity == null && flavorId !=0)
+      {
+        _db.TreatFlavors.Add(new TreatFlavor() { FlavorId = flavorId, TreatId = treat.TreatId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
+
+// CREATE functions ^^^^
+//------------------------------------------------------------------
+// UPDATE functions vvvv
+
+    public ActionResult Edit(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Treat treat)
+    {
+      _db.Treats.Update(treat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+// UPDATE functions ^^^^
+//------------------------------------------------------------------
+// DELETE functions vvvv
+
+
+    public ActionResult Delete(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      TreatFlavor joinEntry = _db.TreatFlavors.FirstOrDefault(e => e.TreatFlavorId == joinId);
+      _db.TreatFlavors.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    // DELETE functions ^^^^
+  }
+}
