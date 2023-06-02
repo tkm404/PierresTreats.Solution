55c5593ad98424257ee13e6ff5e4041b1ae6e2e5
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-06-02 15:25:54 -0700

    add Role Controller.

---
+++ PierresTreats/Controllers/RoleController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PierresTreats.Controllers
+{
+  public class RoleController : Controller{
+    private RoleManager<IdentityRole> roleManager;
+    public RoleController(RoleManager<IdentityRole> roleMgr)
+    {
+      roleManager = roleMgr;
+    }
+    public ViewResult Index() => View(roleManager.Roles);
+
+    private void Errors(IdentityResult result)
+    {
+      foreach (IdentityError error in result.Errors)
+        ModelState.AddModelError("", error.Description);
+    }
+  }
+}
